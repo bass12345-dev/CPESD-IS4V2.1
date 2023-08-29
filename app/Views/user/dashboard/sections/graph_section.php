5187ab62d40b6425be553c93f3b0cc089d64c548
bass12345-dev <manabobasil@gmail.com>
2023-08-28 17:59:56 -1000

    first commit

---
+++ app/Views/user/dashboard/sections/graph_section.php
@@ -0,0 +1,22 @@
+<div class="row mt-2">
+    <div class="col-lg-12 mt-sm-30 mt-xs-30">
+        <div class="card">
+            <div class="card-body">
+                <div class="col-md-6 pull-left "   >
+                        <div class="loader-alert"></div>
+                    </div>
+                    <div class="col-md-6 pull-right "   >
+                        <select class="custom-select" id="user_year" onchange="load_user_graph(this)"  >
+                            <option selected >2023</option>
+                            <option>2024</option>
+                            <option>2025</option>
+                        </select>
+                    </div>
+                <canvas id="user-bar-chart"  height="100"></canvas>
+            </div>
+        </div>
+    </div>
+
+   
+   
+</div>
