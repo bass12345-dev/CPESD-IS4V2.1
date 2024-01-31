c5a67b04fa2a41f74cf644d509773d35f7b76a2f
bass12345-dev <manabobasil@gmail.com>
2024-01-30 23:54:49 -0800

    'update'

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-       return redirect()->to('login');
+       // return redirect()->to('login');
+      return view('maintenance');
     }
 }
